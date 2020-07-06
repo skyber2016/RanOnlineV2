12db46bf365979ea7a6b090249bfc625e2734873
Duy <nguyenhoangduy1997@gmail.com>
2020-07-06 22:33:54 +0700

    Tool deploy

--- deployment.sh
+++ deployment.sh
@@ -10,8 +10,35 @@ export ec2_ppk=./ran_online.ppk
 export rootDirectory=source
 # Echo start deployment
 echo 'Start deployment for '$env' server - '$ec2_host
-# remove folder ignore
-plink -i $ec2_ppk $ec2_host "pm2 delete apiService"
-plink -i $ec2_ppk $ec2_host "cd /home/ubuntu/source_netcore/RanOnlineV2/BE/RanOnlineCore && dotnet restore && dotnet build && pm2 start --name apiService dotnet -- run && pm2 save"
+rm -rf BE/RanOnlineCore/bin
+rm -rf ./Deploy
+mkdir Deploy
+echo 'Buiding back end'
+cd BE/RanOnlineCore && dotnet publish -o ./published -r linux-x64 && cd ../..
+echo 'copy source to server'
+cd Deploy && mkdir BE
+cd ..
+cp -r ./BE/RanOnlineCore/bin/Debug/netcoreapp2.1/linux-x64/ ./Deploy/BE
+echo 'Building front-end'
+cd FE && npm run build && cd ..
+cp -r ./FE/dist/FE/ ./Deploy
+echo 'zip file'
+7z a -tzip deploy.zip ./Deploy/*
+echo 'set permission writeable'
+plink -i $ec2_ppk $ec2_host "mkdir ./avenger_new && sudo chmod 777 ./avenger_new"
+echo 'Copy source code to server'
+pscp -i $ec2_ppk -r ./deploy.zip $ec2_host:/home/ubuntu/avenger_new
+# Backup source current
+echo 'Backup source current'
+plink -i $ec2_ppk $ec2_host "sudo rm -rf ./backup_avenger"
+plink -i $ec2_ppk $ec2_host "sudo mv ./avenger ./backup_avenger"
+# rename source folder
+echo 'rename source folder'
+plink -i $ec2_ppk $ec2_host "sudo mv ./avenger_new ./avenger"
+echo 'unzip'
+plink -i $ec2_ppk $ec2_host "cd avenger && unzip deploy.zip"
+echo "Starting server"
+plink -i $ec2_ppk $ec2_host 'pm2 del api';
+plink -i $ec2_ppk $ec2_host 'cd ./avenger/BE/linux-x64 && pm2 start "dotnet RanOnlineCore.dll" --name "api"';
 echo 'Press [Enter] key to continue...'
 read
